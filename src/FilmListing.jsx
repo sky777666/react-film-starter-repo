e97b0eac68e70f5685d65f87d14ed7f6359f6904
sky777666 <lmyer12@my.wgu.edu>
2020-07-16 09:13:48 -0700

    Luke Myers HWMK

--- src/FilmListing.jsx
+++ src/FilmListing.jsx
@@ -3,6 +3,19 @@ import React, { Component } from 'react';
 import FilmRow from './FilmRow';
 
 class FilmListing extends Component {
+  constructor() {
+    super()
+    this.state = {
+      filter: 'all'
+    }
+  }
+
+handleFilterClick = (filter)=> {
+  this.setState({filter })
+  console.log('Settting filter to' + filter)
+}
+
+
   render() {
     const allFilms = this.props.films.map((film) =>{
       return (
@@ -13,14 +26,17 @@ class FilmListing extends Component {
       <div className="film-list">
       <h1 className="section-title">FILMS</h1>
       <div className="film-list-filters">
-          <div className="film-list-filter">
+
+          <div className={`film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`}
+           onClick={() => this.handleFilterClick('all')}>
               ALL
               <span className="section-count">{this.props.films.length}</span>
           </div>
-          <div className="film-list-filter">
+          <div className={`film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`}
+           onClick={() => this.handleFilterClick('faves')}>
               FAVES
               <span className="section-count">0</span>
-              <button onClick={this.handleFilterClick}></button>
+             
           </div>
       </div>
   
--- src/FilmRow.js
+++ src/FilmRow.js
@@ -28,20 +28,24 @@ import Fave from './fave';
 
 
 class FilmRow extends Component {
+
+handleDetailsClick = (film) => {
+  console.log(`Fetching details for ${film.title} 🐝`)
+}
+
   render() {
     return (
-      <div className="film-row">
+      <div className="film-row" onClick={() => this.handleDetailsClick(this.props.film)}>
         <FilmPoster 
           poster_path={`https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`}
           title={this.props.film.title}
         />
 
         <div className="film-summary">
-        <Fave />
           <h1>{this.props.film.title}</h1>
           <p>{this.props.film.release_date.substr(0,4)}</p>
         </div>
-        
+        <Fave />
       </div>
     );
   }
